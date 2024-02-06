81fbfa2efb2f5abb516de48e3a417d4b9078d28d
paframar <paframar@gmail.com>
2024-02-06 14:53:06 +0100

    Project upload.

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabaseUrl = "https://jmdscupdcnnmoymojkxp.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImptZHNjdXBkY25ubW95bW9qa3hwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcxMzEyODYsImV4cCI6MjAyMjcwNzI4Nn0.RteDyQkXOUXa3MqfAx2KxOxuyZak0W8c69_ULYHvGjM";
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
